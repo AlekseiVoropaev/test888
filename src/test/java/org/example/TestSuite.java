89de7037556c24e369d424ee01557e5122b5f399
AlekseiVoropaev <aleksej.voropaev.93@mail.ru>
2023-07-02 14:09:38 +0300

    Comment Deleted

--- src/test/java/org/example/TestSuite.java
+++ src/test/java/org/example/TestSuite.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
-// comment
+
 @Suite
 @SelectPackages("org.example")
 @ExcludeTags("param")
