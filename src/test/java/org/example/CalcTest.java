8bc1cb77f0c0c63af0cb045978f8c3b766118c19
AlekseiVoropaev <aleksej.voropaev.93@mail.ru>
2023-07-02 16:56:04 +0300

    Metod sub and ParameterizedTest  added

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -26,25 +26,6 @@ class CalcTest {
         System.out.println("After All");
     }
 
-    @Test
-    @DisplayName("Проверка суммирования 1")
-    @Timeout(10)
-    @Tag("summ1")
-    void summ1() {
-        Calc calc = new Calc();
-        int result = calc.summ(1,6);
-        Assertions.assertEquals(7, result , "Не тот ответ");
-
-    }
-   // @RepeatedTest(7)
-    @Test
-    @DisplayName("Проверка суммирования 2")
-    @Timeout(10)
-    void summ2() {
-      Calc calc = new Calc();
-        int result = calc.summ(1, 6);
-        Assertions.assertEquals(7, result, "Не тот ответ");
-    }
 
     @ParameterizedTest(name = " #{index} - сложение {0} и {1}, ожидаем {2}")
     @CsvSource({"1, 2, 3" , "-1, 2, 1", "0, 0, 0 "})
@@ -56,15 +37,13 @@ class CalcTest {
         Assertions.assertEquals(expendedresult, result , "Не тот ответ");
 
     }
-
     @ParameterizedTest(name = " #{index} - вычитание {0} и {1}, ожидаем {2}")
     @CsvSource({"1, 2, -1" , "-1, 2, -3", "0, 0, 0 "})
     @DisplayName("Проверка вычитания")
-    @Tag("sub")
+    @Tag("param")
     void sub(int a,  int b , int expendedresult) {
         Calc calc = new Calc();
         int result = calc.sub(a,b);
         Assertions.assertEquals(expendedresult, result , "Не тот ответ");
     }
-
-    }
+}
--- src/test/java/org/example/TestSuite.java
+++ src/test/java/org/example/TestSuite.java
@@ -1,12 +1,17 @@
 package org.example;
 
 import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages("org.example")
-@ExcludeTags("param")
-public class TestSuite {
+@IncludeTags("param")
+class TestSuite {
 
 }
+
+
+
+
